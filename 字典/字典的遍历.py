2a358d64d4b2686cd2ae45205cf12af094b40e9d
yanxuelei <xueleiyan@jieyuechina.com>
2020-07-06 14:56:53 +0800

    代码优化

--- 字典/字典的遍历.py
+++ 字典/字典的遍历.py
@@ -4,7 +4,7 @@ persion = {'username':'yan','age':29,'sex':'男'}
 # for key in persion.keys():
 #     print(key)
 
-# 使用value方法班里所以的值
+# 使用value方法遍历所以的值
 # for key in persion.values():
 #     print(key)
 
@@ -12,7 +12,7 @@ persion = {'username':'yan','age':29,'sex':'男'}
 items = persion.items()
 # for item in items:
 #     key,value = item #元组解饱
-#     print(key,value)
+    # print(key,value)
 
 for key,value in items:
     print(key,value)
