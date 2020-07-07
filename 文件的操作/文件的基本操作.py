280a21b31e1d71566bccb6e3d57ca2ac638ca0fc
yanxuelei <xueleiyan@jieyuechina.com>
2020-07-07 18:00:17 +0800

    文件操作相关内容

--- 文件的操作/文件的基本操作.py
+++ 文件的操作/文件的基本操作.py
@@ -6,4 +6,13 @@
 @time:2020/7/7 0007
 """
 # 1,打开文件 2,做相关操作 3.关闭文件
-file = open('test.txt')
+# 读取文件 使用r
+# file = open('test.txt','r')
+# str = file.read()
+# print(str)
+# file.close()
+
+#写入文件 使用w  python3编码不是utf-8 因此需要使用encoding
+file = open('test.txt','w',encoding='utf-8')
+file.write('闫学雷')
+file.close()
