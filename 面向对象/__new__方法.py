8c8267bb90022cb4991d6ee8cf89246bd1358f73
yanxuelei <xueleiyan@jieyuechina.com>
2020-07-13 17:51:21 +0800

    单例

---
+++ 面向对象/__new__方法.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+"""
+@author:闫学雷
+@project:PythonYan
+@file: __new__方法.py
+@time:2020/7/13 0013
+"""
---
+++ 面向对象/单例设计模式.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+"""
+@author:闫学雷
+@project:PythonYan
+@file: 单例设计模式.py
+@time:2020/7/13 0013
+"""
