f40ddd1b81041d7b772866cd3b745d6a7cf243c3
yanxuelei <xueleiyan@jieyuechina.com>
2020-07-17 17:31:06 +0800

    1.数据库通过导入功能实现 2.优化写入数据库场景代码

--- other/九九乘法表.py
+++ other/九九乘法表.py
@@ -1,8 +1,9 @@
 for m in range(1,10):
     for n in range(1,10):
         if n <= m:
-            print("%s * %s = %s  "%(n,m,m*n),end='')
-    print()
+            # print("%s * %s = %s  "%(n,m,m*n),end='')
+            pass
+    # print()
 
 
 lie =1
@@ -13,3 +14,7 @@ while lie <=9:
         hang =hang + 1
     lie = lie +1
     # print()
+
+import random
+ran = '621483011226' + str(random.randint(1000,9999))
+print(ran)
--- 文件的操作/readExcel.py
+++ 文件的操作/readExcel.py
@@ -7,38 +7,38 @@
 """
 
 import xlrd
-import pymysql
+import random
+# from PythonYan.数据库.connectMysql import connect_mysql #绝对路径导入文件并使用其中的方法
+from 数据库.connectMysql import connect_mysql
 
-try:
-    db = pymysql.connect(
-        host = 'localhost',
-        port = 3306,
-        user= 'root',
-        password = '123456',
-        db = 'test',
-        charset = 'utf8'
-    )
-    #创建游标
-    cur = db.cursor()
-except Exception as e:
-    print(e)
-else:
-    print('连接成功:{}'.format(cur))
-
-
-def excel():
-    wb = xlrd.open_workbook("D:\python37\学习\捷越直接放款债权明细.xlsx") #打开Excel文件
+def read_excel():
+    wb = xlrd.open_workbook("D:\python37\PythonYan\文件的操作\捷越直接放款债权明细.xlsx") #打开Excel文件
     sheet = wb.sheet_by_name("Sheet1")   #通过excel表格名称(rank)获取工作表
-    for a in range(sheet.nrows):   #循环读取表格内容（每次读取一行数据）
+    print('<<<<<<<<<<<<<<<<<文件开始读取>>>>>>>>>>>>>>>>>>')
+    return sheet
+
+def write_mysql():
+    db = connect_mysql()
+    # 创建游标
+    cur = db.cursor()
+    run = 0
+    sheet = read_excel()
+    for a in range(sheet.nrows):  # 循环读取表格内容（每次读取一行数据）
         cells = sheet.row_values(a)  # 每行数据赋值给cells
         if '*' in cells[3]:
-            cells[3] = '123'
+            cells[3] = '621483011226' + str(random.randint(1000,9999))
+            insert = ("INSERT INTO `excel`(name,card_id,phone,bank_card) VALUES ('%s','%s','%s',%s)" % (
+                cells[0], cells[1], cells[2], cells[3]))
+            cur.execute(insert)
+            db.commit()
+            run = run + 1
+            print('已执行{}行到数据库'.format(run))
+        else:
             insert = ("INSERT INTO `excel`(name,card_id,phone,bank_card) VALUES ('%s','%s','%s',%s)" % (
             cells[0], cells[1], cells[2], cells[3]))
             cur.execute(insert)
             db.commit()
-        else:
-            insert = ("INSERT INTO `excel`(name,card_id,phone,bank_card) VALUES ('%s','%s','%s',%s)" % (cells[0], cells[1], cells[2], cells[3]))
-            cur.execute(insert)
-            db.commit()
-excel()
+            run = run + 1
+            print('已执行{}行到数据库'.format(run))
+    print("<<<<<<<<<<<<执行完毕>>>>>>>>>>>>>>")
+write_mysql()
