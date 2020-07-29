0e1d9a8367e1e652519719ba9506ae2eaaa38bb7
yanxuelei <xueleiyan@jieyuechina.com>
2020-07-29 16:41:59 +0800

    添加接口自动化

--- python接口测试/test.py
+++ python接口测试/test.py
@@ -1,9 +1,17 @@
-#!/usr/bin/env python 
-"""
-@author:闫学雷
-@project:PythonYan
-@file: test.py
-@time:2020/7/29 0029
-"""
-
 import requests
+
+r=requests.get('http://t.weather.itboy.net/api/weather/city/101250101')
+response_data=r.json()
+print(response_data)
+
+#获取日期，响应信息，状态，城市
+print(response_data['date'])
+print(response_data['message'])
+print(response_data['status'])
+print(response_data['cityInfo']['city'])
+
+#获取当日天气具体信息
+print(response_data['data']['forecast'][0]['ymd'])
+print(response_data['data']['forecast'][0]['type'])
+print(response_data['data']['forecast'][0]['high'])
+print(response_data['data']['forecast'][0]['low'])
