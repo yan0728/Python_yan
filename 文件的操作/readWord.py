b73621ba2816cbeab46e36f293bc417e8edce312
yanxuelei <xueleiyan@jieyuechina.com>
2020-12-29 16:54:46 +0800

    更新文件

--- 文件的操作/readWord.py
+++ 文件的操作/readWord.py
@@ -8,7 +8,11 @@
 
 
 import docx
+from 数据库.connectMysql import connect_mysql
 
+db = connect_mysql()
+
+cur = db.cursor()
 file = docx.Document("111.docx")
 
 # docx 仅支持.docx的文件，直接通过doc后缀改成docx也不好用
@@ -19,4 +23,8 @@ file = docx.Document("111.docx")
 
 #输出段落编号及段落内容
 for i in range(len(file.paragraphs)):
-    print("第"+str(i)+"段的内容是："+file.paragraphs[i].text)
+    inset = ('INSERT INTO test.`testcase` (dl) VALUE ("{}")'.format(file.paragraphs[i].text))
+    cur.execute(inset)
+    db.commit()
+    # print("第"+str(i)+"段的内容是："+file.paragraphs[i].text)
+print("完成")
