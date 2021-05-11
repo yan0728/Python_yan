bd2c359b107ddf410330a004707e0195ea61dbb8
yan0728 <yanxuelei02@163.com>
2021-05-11 16:19:26 +0800

    2021-5-11 公司更新

--- zn/中国移动/test.py
+++ zn/中国移动/test.py
@@ -1,11 +1,13 @@
 from faker import Faker
 import time
+import random
+
+fileNo = 'FL' + str(random.randint(10000,99999))
 faker = Faker(locale='zh_CN')
 
 
-address = time.strftime()
-address1 = faker.name()
 
-a = faker.numerify()
-# time = time.strftime("%Y")
-print(address)
+time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+No = 'NO' + time
+address1 = faker.name()
+print(time,No,fileNo)
---
+++ zn/中国移动/中移动造推送附件数据.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import random
+import time
+
+time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+flowNo = 'NO' + time
+fileNo = 'FL' + str(random.randint(10000,99999))
+
+def creatData():
+    header = {'Content-Type':'application/json;charset=UTF-8','User-Agent': 'Apache-HttpClient/4.5.5 (Java/1.8.0_121)','Connection':'keep-alive'}
+    url = 'https://test-api-yidong.lianjieabs.com/asset/asset_repair/assetRepair'
+
+    data = {
+      "channel": "渠道编号",
+      "flowNo": flowNo,
+      "fileRepairFO": {
+       "addr": "yidong/20210511/180000/FL00041_20210203-000907-F.zip",
+       "assets": ["HBD003"],
+       "extension": "pdf",
+       "fileNo": fileNo,
+       "fileSize": 20480,
+       "fileType": "00",
+       "filename": fileNo + "_20210203-000907-F.zip",
+       "loanAmountTotal": 30,
+       "requestId": ""
+     },
+     "time": time,
+     "version": "1.1.0"
+    }
+
+    r = requests.post(url=url,data=json.dumps(data) ,headers=header)
+    print("<<<<<<<请求信息>>>>>>>")
+    print(json.dumps(r.json(),sort_keys=True, indent=2,ensure_ascii = False))
+    return r.json()
+
+def requestData():
+    header = {'Content-Type': 'application/json;charset=UTF-8',
+              'User-Agent': 'Apache-HttpClient/4.5.5 (Java/1.8.0_121)', 'Connection': 'keep-alive'}
+    url = 'https://test-api-yidong.lianjieabs.com/external/resources'
+
+    data = creatData()
+    r = requests.post(url=url,data=json.dumps(data) ,headers=header)
+    # 打印的返回值是json格式
+    print("<<<<<<<<<<<<<<<<<<推送response>>>>>>>>>>>>>")
+    print(json.dumps(r.json(),sort_keys=True, indent=2,ensure_ascii = False))
+
+requestData()
