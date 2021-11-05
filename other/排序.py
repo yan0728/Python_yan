627899e6e7ea78cb0c4b5a9ea0986b1839298640
yan0728 <yanxuelei02@163.com>
2021-11-05 11:36:16 +0800

    ADD 添加排序代码

--- other/排序.py
+++ other/排序.py
@@ -1,7 +1,18 @@
 li = [56,44,67,28,68,79,3]
 
-for j in range(0,len(li)-1):
-    for i in range(0,len(li)-1):
-        if li[i] > li[i+1]:
-            li[i],li[i+1] = li[i+1],li[i]
-print(li)
+def PaiXu(list):
+    for j in range(0,len(li)-1):
+        for i in range(0,len(li)-1):
+            if li[i] > li[i+1]:
+                li[i],li[i+1] = li[i+1],li[i]
+    print(li)
+
+PaiXu(li)
+
+def QiuHe(list):
+    sum = 0
+    for i in range(0,len(li)):
+        sum = sum + li[i]
+    print(sum)
+
+QiuHe(li)
